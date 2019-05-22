00804dd344cd24cc63d5f6ca98b395790e2256d5
Washington Correia <37412752+washingtonklebio@users.noreply.github.com>
2019-05-22 14:05:12 -0300

    Update Functions.js

--- JavaScript/Functions.js
+++ JavaScript/Functions.js
@@ -7,3 +7,22 @@
 function removerAcentos(str) {
   return str.normalize('NFD').replace(/[\u0300-\u036f]/g, "")
 }
+
+# Buscar campos de tabela dinamicamente jquery
+$(document).on('keyup', '#input_busca', function() {
+    let valor = $(this).val();
+    if(valor != ""){
+        $(".pesquisa").hide();
+        let reg = new RegExp(valor, "ig");
+        $(".pesquisa").each(function(){
+            let nome = $(this).attr('nome');
+            let user = $(this).attr('user');
+            if (reg.test(nome) || reg.test(user)) {
+                $(this).show();
+            }
+        });
+    }else {
+        // Mostra todos as <tr>
+        $(".pesquisa").show();
+    }
+});
