e587396e6b23c70c64241eb0e64a723d0cd046be
Washington Correia <37412752+washingtonklebio@users.noreply.github.com>
2019-11-26 17:22:47 -0300

    Update mysql.sql

--- MySQL/mysql.sql
+++ MySQL/mysql.sql
@@ -1,3 +1,8 @@
 # Stop no banco de dados Mysql
 sudo systemctl stop mysql
 
+# Exportar apenas a estrutura do banco de dados
+mysqldump -d -u <user> -p <database> > <arquivo.sql>
+
+# Importação da estrutura gerada para o banco de dados
+mysql -u <user> <database> < <arquivo.sql>
